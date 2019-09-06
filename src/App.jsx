c3d5026ffdad9e2a39551dedb90732a6ffebf6e7
Olivia <olivia.f16@unitec.edu>
2019-09-06 17:45:40 -0600

    Laboratorio 8 Olivia Fernandez

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import Item from './components/Item';
+import Button from './components/Button';
+import _ from 'lodash';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [{
+                image: "hambuerger.png",
+                name: "Jumbo burger",
+                portion: "500g",
+                price: 40
+            }]
+        }
+    }
+
+    _remove(position) {
+        let { data } = this.state;
+
+        let newData = [
+            ...data.slice(0, position),
+            ...data.slice(position + 1),
+        ]
+
+        this.setState({  data: newData });
+
+    }
+
+    _add() {
+        let { data } = this.state;
+        let newData = [
+            ...data,
+            {
+                image: "papas.png",
+                name: " Curumba fries",
+                portion: "140g",
+                price: Math.floor(Math.random() * 20)
+            }
+        ]
+        this.setState({  data: newData });
+    }
+
+    _getTotal() {
+        return _.sumBy(this.state.data, function(o) { return o.price; });;
+    }
+
+
+    render() {
+        return ( <div className = "app" >
+            <h1 > Ejemplo de listas </h1> <ul className = "todo-list" > {
+                this.state.data.map(
+                    (item, index) =>
+                    <
+                    Item data = { item }
+                    key = { index }
+                    onRemove = {
+                        () => this._remove(index)
+                    }
+                    />
+                )
+            } </ul> <div className = "footer" >
+            <
+            Button onClick = { this._add.bind(this) }
+            name = "Add item" /
+            >
+            < h4 > $ { this._getTotal() } </h4> </div> </div>
+        );
+    }
+}
+
+export default App;
